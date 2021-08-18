8660d528399dfb697a88f50f1d318f9e8264c732
jaehyuns <83747541+jaehyuns@users.noreply.github.com>
2021-08-18 11:54:33 +0900

    Add files via upload
    외접원 함수 추가

--- test.py
+++ test.py
@@ -40,10 +40,77 @@ class point:
 		self.center_y = None
 		self.xycar_angle = None
 		self.xycar_angle_deg = None
+	
 
+	def circles_4_center(self,sorted_list):
+		sorted_2_list=None
+		sorted_2_list=sorted(sorted_list,key= lambda circle:circle.center.y)
+		return sorted_2_list
 	def get_center(self, obstacles):
 		self.obData = obstacles	
-		
+	
+	
+	def calcEquidistance(self,x1, x2, x3, y1, y2, y3):
+    		xmAB = (x1 + x2) / 2
+		ymAB = (y1 + y2) / 2
+		xmBC = (x3 + x2) / 2
+	 	ymBC = (y3 + y2) / 2
+		xmAC = (x3 + x1) / 2
+		ymAC = (y3 + y1) / 2
+
+	    	yDiffAB = y2 - y1
+	    	xDiffAB = x2 - x1
+	    	yDiffBC = y3 - y2
+	    	xDiffBC = x3 - x2
+	    	yDiffAC = y3 - y1
+	    	xDiffAC = x3 - x1
+
+    		if yDiffAB != 0 and xDiffAB != 0:
+        		a = -1 / (yDiffAB / xDiffAB)
+        		a1 = ymAB - (a*xmAB)
+
+    		if yDiffBC != 0 and xDiffBC != 0:
+        		b = -1 / (yDiffBC / xDiffBC)
+        		b1 = ymBC - (b*xmBC)
+
+    		if yDiffAC != 0 and xDiffAC != 0:
+        		c = -1 / (yDiffAC / xDiffAC)
+        		c1 = ymAC - (c*xmAC)
+
+    		if yDiffAB != 0 and xDiffAB != 0 and yDiffBC != 0 and xDiffBC != 0 and yDiffAC != 0 and xDiffAC != 0:
+        		self.center_x = -((a1 + (-b1)) / ((-b) + a))
+        		self.center_y= (b * self.center_x) + b1
+
+        		print("\nPonto equidistante:", [self.center_x, self.center_y])
+
+			return self.center_x,self.center_y
+
+    		elif yDiffAB != 0 and xDiffAB != 0 and yDiffBC != 0 and xDiffBC != 0:
+        		self.center_x = -((a1 + (-b1)) / ((-b) + a))
+        		self.center_y = (b * self.center_x) + b1
+
+        		print("\nPonto equidistante:", [self.center_x, self.center_y])
+
+			return self.center_x,self.center_y
+
+
+    		elif yDiffBC != 0 and xDiffBC != 0 and yDiffAC != 0 and xDiffAC != 0:
+        		self.center_x = -((b1 + (-c1)) / ((-c) + b))
+        		self.center_y = (c * self.center_x) + c1
+
+        		print("\nPonto equidistante:", [self.center_x, self.center_y])
+
+			return self.center_x,self.center_y
+
+
+    		elif yDiffAB != 0 and xDiffAB != 0 and yDiffAC != 0 and xDiffAC != 0:
+        		self.center_x = -((c1 + (-a1)) / ((-a) + c))
+        		self.center_y = (a * self.center_x) + a1
+
+        		print("\nPonto equidistante:", [self.center_x, self.center_y])
+	
+			return self.center_x,self.center_y
+
 	
 
 	def circle(self):
@@ -56,10 +123,10 @@ class point:
 		elif len(self.obData.circles) == 1:
 			circles=self.obData.circles
 			if(circles[0].center.y > 0.0003):
-				drive(20,2)
+				drive(-25,3)
 				print("aa")	
 			elif(circles[0].center.y < 0.0003):
-				drive(-20,2)
+				drive(25,3)
 				print("bb")
 
 
@@ -69,10 +136,53 @@ class point:
 			left_circle=None
 			right_circle=None
 			sorted_list=None
-
-		
+			a=1.4
+	                b=0.7
 			if(len(circles)>2):
 				sorted_list=sorted(circles,key= lambda circle:circle.center.x) # near(x-axis) 2 Circle
+				if(len(circles)==3):
+					sorted_2_list=self.circles_4_center(sorted_list)
+					filter_point1=sorted_2_list[0].center
+					filter_point2=sorted_2_list[1].center
+					filter_point3=sorted_2_list[2].center
+					
+
+					self.center_x,self.center_y=self.calcEquidistance(filtered_point1.x,filtered_point2.x,filtered_point3.x,filtered_point1.y,filtered_point2.y,filtered_point3.y)
+					self.calc_angle()
+					drive(self.xycar_angle_deg,4)
+					
+
+					#if(filter_point3.y<0):
+					#	drive(18,3)
+						
+					#else:
+					#	if(filter_point2.y<0):
+					#		drive(13,3)					
+					#	else:
+					#		drive(-13,3)
+					
+
+						
+								
+
+
+				if(len(circles)>3):
+					sorted_2_list=self.circles_4_center(sorted_list)					
+					left_circle_1=sorted_2_list[0]
+					left_circle_2=sorted_2_list[1]
+					right_circle_1=sorted_2_list[2]
+					right_circle_2=sorted_2_list[3]
+					left_point1=left_circle_1.center
+					left_point2=left_circle_2.center
+					right_point1=right_circle_1.center
+					right_point2=right_circle_2.center
+					self.center_x=(left_point1.x+left_point2.x+right_point1.x+right_point2.x)/4
+					self.center_y=(left_point1.y+left_point2.y+right_point1.y+right_point2.y)/4
+					self.calc_angle()
+					drive(self.xycar_angle_deg,4)
+					return
+				
+				
 			
 				filtered_circle1=sorted_list[0]
 				filtered_circle2=sorted_list[1]
@@ -86,12 +196,29 @@ class point:
 						
 						
 			elif(len(circles)==2):
+				
 				if(circles[0].center.y<circles[1].center.y):
 					left_circle=circles[0]
 					right_circle=circles[1]
+					
 				else:
 					left_circle=circles[1]
 					right_circle=circles[0]
+                                        
+
+					
+				
+				if(right_circle.center.y<0):
+					self.xycar_angle_deg=20	
+ 					drive(self.xycar_angle_deg,3)
+					
+					return
+				elif(left_circle.center.y>0):
+					self.xycar_angle_deg=-20
+					drive(self.xycar_angle_deg,3)
+					
+					return
+					
 				print("left_circle:",left_circle.center.y,"right_circle:",right_circle.center.y)
 
 					
@@ -104,7 +231,7 @@ class point:
 			self.calc_angle()
 		
 			print("x:",self.center_x,"y:",self.center_y,"deg:",self.xycar_angle_deg)
-		        drive(self.xycar_angle_deg, 3)
+		        drive(self.xycar_angle_deg,4)
 
 			#self.publish_angle()
 			
@@ -113,7 +240,7 @@ class point:
 		self.xycar_angle = math.atan2(self.center_y,self.center_x)
 		self.xycar_angle_deg = self.xycar_angle*180/math.pi #obstacles chase
 		if (self.xycar_angle_deg > 30): self.xycar_angle_deg = 30
-		elif (self.xycar_angle_deg < -30): self.xycar_angle_deg = 30
+		elif (self.xycar_angle_deg < -30): self.xycar_angle_deg = -30
 		print("Angle deg : ", self.xycar_angle_deg)
 
 
@@ -128,3 +255,4 @@ if __name__=='__main__':
 		ob.circle()
 	
 	print('Done')
+	
